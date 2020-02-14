4f611f5f540478e66b05da99b10aad14e3cbd856
Silicca <floriane.carton.etu@univ-lille.fr>
2020-02-14 11:42:09 +0100

    change constructor, add and delete function - add documentation

--- src/Entity/Menu.php
+++ src/Entity/Menu.php
@@ -1,31 +1,34 @@
 <?php
 
+/** 
+*   This file contains the class entity for the menus
+*/
+
 declare(strict_types=1);
 
 namespace App\Entity;
 
-class Menu
+use App\Entity\IBuyable;
+
+/** 
+*   Menu class
+*/
+class Menu implements IBuyable
 {
     private $id;
-    private $products;
+
+    private $menu;
+
     private $name;
+
     private $price;
 
-    public function __construct()
+    public function __construct($name, $price)
     {
         $this->id = \App\ORM\Util\UUID::v4();
         $this->products = [];
-    }
-
-    public function addProduct($product): int
-    {
-        return array_push($this->products, $product);
-    }
-
-    public function deleteProduct($product): void
-    {
-        $key = array_search($product, $this->products);
-        unset($this->products[$key]);
+        $this->name = $name;
+        $this->price = $price;
     }
 
     public function getId(): string
@@ -33,19 +36,14 @@ class Menu
         return $this->id;
     }
 
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
     public function getName(): string
     {
         return $this->name;
-    }
-
-    public function getPrice(): int
-    {
-        return $this->price;
-    }
-
-    public function getProducts()
-    {
-        return $this->products;
     }
 
     public function setPrice($price): void
@@ -53,13 +51,36 @@ class Menu
         $this->price = $price;
     }
 
-    public function setName($name): void
+    public function getPrice(): int
     {
-        $this->name = $name;
+        return $this->price;
     }
 
-    public function setProducts($products): void
+    public function getMenu() : array
     {
-        $this->products = $products;
+        return $this->menu;
+    }
+
+    /**
+    * This function add a product to a menu
+    *
+    * @param IBuyable $buyable The product added to the menu
+    * @return void
+    */
+    public function add(IBuyable $buyable): void
+    {
+        $id = $buyable->getId();
+        array_push($this->menu, $id);
+    }
+
+    /**
+    * This function delete a product of a menu
+    *
+    * @param IBuyable $buyable The product deleted of the menu
+    * @return void
+    */
+    public function delete(IBuyable $buyable): void
+    {
+        unset($this->getMenu()[$buyable->getId()]);
     }
 }
