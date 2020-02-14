38af31e76ffc7245e2447ac8b92925521f7337ca
Julien SERRURIER <julien.serrurier.etu@univ-lille.fr>
2020-02-14 09:49:16 +0100

    refacto(Entity): add getters/setters for Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,10 +4,53 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Entity\CategorieEnum;
+
 class Product
 {
-    public $id;
-    public $name;
-    public $price;
-    public $cat;
+    private $id;
+    private $name;
+    private $price;
+    private $cat;
+
+    public function __construct()
+    {
+        $this->id = \App\ORM\Util\UUID::v4();
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setCat(CategorieEnum $cat)
+    {
+        $this->cat = $cat;
+    }
+
+    public function getCat(): CategorieEnum
+    {
+        return $this->cat;
+    }
+
 }
