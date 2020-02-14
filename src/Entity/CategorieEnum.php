d4357e2b3a25ce9b06d61d71f291378e2d2d9761
Julien SERRURIER <julien.serrurier.etu@univ-lille.fr>
2020-02-14 09:30:30 +0100

    add Product and CategorieEnum Entities

---
+++ src/Entity/CategorieEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+final class CategorieEnum
+{
+    static public $BURGER = 1;
+    static public $BOISSON = 2;
+    static public $ACCOMPAGNEMENT = 3;
+    static public $SAUCES = 4;
+    static public $DESSERT = 5;
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Product
+{
+    public $id;
+    public $name;
+    public $price;
+    public $cat;
+}
